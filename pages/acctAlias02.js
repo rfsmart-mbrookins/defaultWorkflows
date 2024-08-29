a6d7ea516561bcd683025e7f2d42382d9c73213d
Michael Brookins <michael.brookins@rfsmart.com>
2024-08-29 09:27:41 -0400

    added default workflow happy path for Label Print 01 as well as cleanup

--- pages/acctAlias02.js
+++ pages/acctAlias02.js
@@ -5,54 +5,72 @@ export class AcctAlias02 {
     this.page = page;
   }
 
- 
-  accountAliasIssue02Link = () => this.page.locator('a[id="AccountAliasIssue02_Default"]');
-  textBox = () => this.page.locator('#txt');
-  exitBtn = () => this.page.locator('#btnF3')
+  accountAliasIssue02Link = () =>
+    this.page.locator('a[id="AccountAliasIssue02_Default"]');
+  textBox = () => this.page.locator("#txt");
+  exitBtn = () => this.page.locator("#btnF3");
 
-    //Account Alias 02 link
-    async validateAcctAlias02Link() {
-      await this.accountAliasIssue02Link().click();
-    }
+  //Account Alias 02 link
+  async validateAcctAlias02Link() {
+    await this.accountAliasIssue02Link().click();
+  }
 
-    //Collect Org
-    async collectOrg() {
-      await this.textBox().fill("002");
-      await this.textBox().press('Enter');
-    }
+  //Collect Org
+  async collectOrg() {
+    await this.textBox().fill("002");
+    await this.textBox().press("Enter");
+  }
 
-    async collectAccountSource() {
-      await this.textBox().fill("US");
-      await this.textBox().press('Enter');
-    }
+  //Collect Account Source
+  async collectAccountSource() {
+    await this.textBox().fill("US");
+    await this.textBox().press("Enter");
+  }
 
-    async collectItemNumber() {
-      await this.textBox().fill("AS65001");
-      await this.textBox().press('Enter');
-    }
+  //Collect Item Number
+  async collectItemNumber() {
+    await this.textBox().fill("AS65001");
+    await this.textBox().press("Enter");
+  }
 
-    async collectSubinventory() {
-      await this.textBox().fill("Stores");
-      await this.textBox().press('Enter');
-    }
-    async collectLocator() {
-      await this.textBox().fill("1.1.1.1");
-      await this.textBox().press('Enter');
-    }
-    async collectQuantity() {
-      await this.textBox().fill("1");
-      await this.textBox().press('Enter');
-    }
-    async collectUom() {
-      await this.textBox().fill("Ea");
-      await this.textBox().press('Enter');
-    }
-    async collectQtyLabels() {
-      await this.textBox().fill("0");
-      await this.textBox().press('Enter');
-    }
-    async exitWorkflow() {
-        await this.exitBtn().click();
-    }
+  //Collect Subinventory
+  async collectSubinventory() {
+    await this.textBox().fill("Stores");
+    await this.textBox().press("Enter");
+  }
 
+  //Collect Locator
+  async collectLocator() {
+    await this.textBox().fill("1.1.1.1");
+    await this.textBox().press("Enter");
+  }
+
+  //Collect Quanity
+  async collectQuantity() {
+    await this.textBox().fill("1");
+    await this.textBox().press("Enter");
+  }
+
+  //Collect Unit of Measure
+  async collectUom() {
+    await this.textBox().fill("Ea");
+    await this.textBox().press("Enter");
+  }
+
+  //Collect Quantity of Labels
+  async collectQtyLabels() {
+    await this.textBox().fill("1");
+    await this.textBox().press("Enter");
+  }
+
+  //Collect Printer -
+  async collectPrinter() {
+    await this.textBox().fill("\\PRINT-SERVLexmark-room");
+    await this.textBox().press("Enter");
+  }
+
+  //Exit Workflow
+  async exitWorkflow() {
+    await this.exitBtn().click();
+  }
 }
--- tests/workflowSmokeTests.spec.js
+++ tests/workflowSmokeTests.spec.js
@@ -1,78 +1,120 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../pages/login";
-import { AcctAlias02 } from "../pages/acctAlias02"; // Ensure correct import
-
+import { AcctAlias02 } from "../pages/acctAlias02";
+import { LabelPrint01 } from "../pages/labelPrint01";
 
 test.beforeEach(async ({ page }) => {
   const loginPage = new LoginPage(page);
+  //Go to Login Page
   await test.step("Go to Login Page", async () => {
     await loginPage.goto();
+  });
+  //Enter Login Credentials
+  await test.step("Input Valid Credentials and Submit", async () => {
+    await loginPage.inputValidLoginCredentials();
+    await loginPage.submitLoginCredentials();
+  });
+  //Click on Regression Link
+  await test.step("Click Regression Link", async () => {
+    await loginPage.validateRegressionLink();
   });
 });
 
 test.describe("Default Workflow Tests", () => {
-
-  /* Login Validation Test - Successful Login */
-  test("Validate Successful Login", async ({ page }) => {
-    const loginPage = new LoginPage(page);
-
-    await test.step("Input Valid Credentials and Submit", async () => {
-      await loginPage.inputValidLoginCredentials();
-      await loginPage.submitLoginCredentials();
-    })
-  });
-
-  /* Test to Open Regression Link */
-  test("Default Workflow Smoketest", async ({ page }) => {
+  
+  /* Account Alias Issue 02 */
+  test("Account Alias Issue 02", async ({ page }) => {
     const loginPage = new LoginPage(page);
     const acctAlias02Page = new AcctAlias02(page);
-
-    // Log in to the application
-    await test.step("Input Valid Credentials and Submit", async () => {
-      await loginPage.inputValidLoginCredentials();
-      await loginPage.submitLoginCredentials();
-    });
-
-    // Click on the Regression Link
-    await test.step("Click Regression Link", async () => {
-      await loginPage.validateRegressionLink(); 
-    });
-  
-    // Click on the Account Alias Issue 02 Link
+    //Click on Account Alias Issue 02 Link
     await test.step("Click Account Alias 02 Link", async () => {
-      await acctAlias02Page.validateAcctAlias02Link(); 
+      await acctAlias02Page.validateAcctAlias02Link();
     });
     //Enter Organization Code
     await test.step("Enter Org Code", async () => {
-      await acctAlias02Page.collectOrg(); 
+      await acctAlias02Page.collectOrg();
     });
+    //Enter Account Source
     await test.step("Enter Account Source", async () => {
       await acctAlias02Page.collectAccountSource();
     });
+    //Enter Item Number
     await test.step("Enter Item Number", async () => {
       await acctAlias02Page.collectItemNumber();
     });
+    //Enter Subinventory
     await test.step("Enter Subinventory", async () => {
       await acctAlias02Page.collectSubinventory();
     });
+    //Enter Locator
     await test.step("Enter Locator", async () => {
       await acctAlias02Page.collectLocator();
     });
+    //Enter Quantity
     await test.step("Enter Quantity", async () => {
       await acctAlias02Page.collectQuantity();
     });
+    //Enter Unit of Measure
     await test.step("Enter UOM", async () => {
       await acctAlias02Page.collectUom();
     });
+    //Enter Quantity of Labels
     await test.step("Enter Quantity of Labels", async () => {
       await acctAlias02Page.collectQtyLabels();
     });
+    //Enter Printer
+    await test.step("Enter Printer", async () => {
+      await acctAlias02Page.collectPrinter();
+    });
+    //Exit Workflow
     await test.step("Exit Workflow", async () => {
       await acctAlias02Page.exitWorkflow();
     });
-
-  
-
   });
 
-});
+  /* Label Print 01 */
+  test("Label Print 01", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const labelPrint01Page = new LabelPrint01(page);
+    // Click on the Label Print 01 Link
+    await test.step("Click Account Alias 02 Link", async () => {
+      await labelPrint01Page.validateLabelPrint01Link();
+    });
+    //Enter Organization Code
+    await test.step("Enter Org Code", async () => {
+      await labelPrint01Page.collectOrg();
+    });
+    //Enter Subinventory
+    await test.step("Enter Subinventory", async () => {
+      await labelPrint01Page.collectSubinventory();
+    });
+    //Enter Locator
+    await test.step("Enter Locator", async () => {
+      await labelPrint01Page.collectLocator();
+    });
+    //Enter Item Number
+    await test.step("Enter Item Number", async () => {
+      await labelPrint01Page.collectItemNumber();
+    });
+    //Enter Quantity
+    await test.step("Enter Quantity", async () => {
+      await labelPrint01Page.collectQuantity();
+    });
+    //Enter Quantity of Labels
+    await test.step("Enter Quantity of Labels", async () => {
+      await labelPrint01Page.collectQtyLabels();
+    });
+    //Enter Printer
+    await test.step("Enter Printer", async () => {
+      await labelPrint01Page.collectPrinter();
+    });
+    //Exit Workflow
+    await test.step("Exit Workflow", async () => {
+      await labelPrint01Page.exitWorkflow();
+    });
+  });
+
+
+
+
+}); //test descibe close
